4c294c0b136690662ea7f555e662291e5b8168b3
Thang Hoang <lnb_Thang@yahoo.com>
2015-10-09 19:28:18 +0700

    edit search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
 	mount_uploader :map, MapUploader
-	scope :title, -> (title) { where("posts.title ilike ?", "#{title}%")}
-	scope :categories, -> (categories) { where("posts.categories ilike ?", "#{categories}%")}
-	scope :salary, -> (salary) { where("posts.salary ilike ?", "#{salary}%")}
-	scope :location, -> (location) { where("posts.location ilike ?", "#{location}%")}
-	scope :position, -> (position) { where("posts.position ilike ?", "#{position}%")}
+	scope :title, -> (title) { where("posts.title like ?", "#{title}%")}
+	scope :categories, -> (categories) { where("posts.categories like ?", "#{categories}%")}
+	scope :salary, -> (salary) { where("posts.salary like ?", "#{salary}%")}
+	scope :location, -> (location) { where("posts.location like ?", "#{location}%")}
+	scope :position, -> (position) { where("posts.position like ?", "#{position}%")}
 	default_scope -> { order(id: :desc) }
 	belongs_to :user
 	has_many :resumes
